831e4b405ddf7f1d7bae80a386013cbcb15f4dc7
Howard Wu <antiair.tw@gmail.com>
2017-08-19 09:58:59 +0800

    add all ordinary files

---
+++ delete.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+?>
+<?php include "db.ini.php"; ?>
+<?php
+$stmt = $db->prepare("UPDATE members SET member_active=:mact WHERE member_id=:mid");
+$stmt->execute(array(
+		":mact" => False,
+		":mid" => $_SESSION['id']
+	));
+
+// 登出
+unset($_SESSION['id']);
+session_destroy();
+setcookie(session_name(),'',time()-100000);
+header("Location: index.php");
+exit;
+?>
---
+++ file_proc.php
@@ -0,0 +1,8 @@
+<?php 
+
+foreach($_FILES['user_file'] as $key => $value)
+	echo $key, ": ", $value, "<br>";
+
+
+
+?>
